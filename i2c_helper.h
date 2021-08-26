80cf6860d6b702aee3a8acabfca86ef0dad0810c
ShadowPhoenix <shadowp@gmail.com>
2021-08-25 23:13:13 -0700

    compile fixes include

--- i2c_helper.h
+++ i2c_helper.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdlib.h>
+#include <cstdint>
 
 
 int communicate_I2C(uint8_t device_address,bool write_comm, uint8_t register_address, uint8_t* recv_buff, int8_t num_of_bytes);
