3ea9597a86444bbfb5a50d720592bd87bbbb9b0e
ShadowPhoenix <shadowp@gmail.com>
2022-03-27 14:34:28 -0700

    fixes to sfa30x

--- SFA30x.cpp
+++ SFA30x.cpp
@@ -41,23 +41,27 @@ int sfa3x_stop()
 
 int sfa3x_read(float* hcho, float* humidity,float* temperature)
 {
-    uint8_t wbuf[4];
-    uint8_t rbuf[8];
-    wbuf[0] = 0x03;
-    wbuf[1] = 0x27;
-    int t_err = write_read_I2C(SFA30x_ADDR, wbuf,2,rbuf,8);
-
+    uint8_t buf[8];
+    buf[0] = 0x27;
+    int t_err = communicate_I2C(SFA30x_ADDR,true,0x03,buf,1);
     if(t_err != 0)
     {
-        printf("Could not write to read out SFA30x! %i\n", t_err);
+        printf("Could not write command to read out SFA30x! %i\n", t_err);
+        return -1;
+    }
+    sleep_ms(10);
+    t_err = read_I2C(SFA30x_ADDR,buf,8);
+    if(t_err != 0)
+    {
+        printf("Could not read out SFA30x! %i\n", t_err);
         return -1;
     }
 
-    int16_t temp_value = ((uint16_t)rbuf[0] << 8 | rbuf[1]); 
+    int16_t temp_value = ((uint16_t)buf[0] << 8 | buf[1]); 
     *hcho = temp_value / 5.0; 
-    temp_value = ((uint16_t)rbuf[3] << 8 | rbuf[4]); 
+    temp_value = ((uint16_t)buf[3] << 8 | buf[4]); 
     *humidity = temp_value / 100.0;
-    temp_value = ((uint16_t)rbuf[6] << 8 | rbuf[7]); 
+    temp_value = ((uint16_t)buf[6] << 8 | buf[7]); 
     *temperature = temp_value / 200.0;
 
     return 0;
