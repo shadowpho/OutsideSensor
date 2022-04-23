d4ca0a24d4e789fe7c6245c5f53c3c2686e215b6
George Karavaev <shadowp@gmail.com>
2022-04-22 23:35:17 -0700

    works for now, figure out 100ms

--- BSECglue.cpp
+++ BSECglue.cpp
@@ -13,10 +13,8 @@
 static struct bme68x_dev gas_sensor;
 int64_t next_call_ns;
 
-static int _rslt; 
-
-#define CHK_RTN_BSEC(x) _rslt = (x); if(_rslt!=0){printf("BSEC Error!%s, %i\n",#x,_rslt);return _rslt;}
-#define CHK_RTN_BME(x) _rslt = (x); if(_rslt!=0){printf("BME Error!%s, %i\n",#x,_rslt);return _rslt;}
+#define CHK_RTN_BSEC(x) do{int _rslt = (x); if(_rslt!=0){printf("BSEC Error!%s, %d\n",#x,_rslt);return _rslt;}}while(0)
+#define CHK_RTN_BME(x) do{int _rslt = (x); if(_rslt!=0){printf("BME Error!%s, %d\n",#x,_rslt);return _rslt;}}while(0)
 
 int BSEC_BME_selftest()
 {
@@ -43,20 +41,20 @@ int BSEC_BME_init()
 
     std::vector<uint8_t> work_buffer(BSEC_MAX_PROPERTY_BLOB_SIZE);
 
-   CHK_RTN_BSEC(bsec_set_configuration(bsec_config_iaq, BSEC_MAX_PROPERTY_BLOB_SIZE, work_buffer.data(), work_buffer.size()));
+   CHK_RTN_BSEC(bsec_set_configuration(bsec_config_selectivity, BSEC_MAX_PROPERTY_BLOB_SIZE, work_buffer.data(), work_buffer.size()));
 
 
     bsec_sensor_configuration_t requested_virtual_sensors[4];
     uint8_t n_requested_virtual_sensors = 4;
 
-    requested_virtual_sensors[0].sensor_id = BSEC_OUTPUT_STATIC_IAQ;
-    requested_virtual_sensors[0].sample_rate = BSEC_SAMPLE_RATE_HIGH_PERFORMANCE;
+    requested_virtual_sensors[3].sensor_id = BSEC_OUTPUT_IAQ;
+    requested_virtual_sensors[3].sample_rate = BSEC_SAMPLE_RATE_LP;
     requested_virtual_sensors[1].sensor_id = BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE;
-    requested_virtual_sensors[1].sample_rate = BSEC_SAMPLE_RATE_HIGH_PERFORMANCE;
+    requested_virtual_sensors[1].sample_rate = BSEC_SAMPLE_RATE_LP;
     requested_virtual_sensors[2].sensor_id = BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY;
-    requested_virtual_sensors[2].sample_rate = BSEC_SAMPLE_RATE_HIGH_PERFORMANCE;
-    requested_virtual_sensors[3].sensor_id = BSEC_OUTPUT_RAW_PRESSURE;
-    requested_virtual_sensors[3].sample_rate = BSEC_SAMPLE_RATE_HIGH_PERFORMANCE;
+    requested_virtual_sensors[2].sample_rate = BSEC_SAMPLE_RATE_LP;
+    requested_virtual_sensors[0].sensor_id = BSEC_OUTPUT_RAW_PRESSURE;
+    requested_virtual_sensors[0].sample_rate = BSEC_SAMPLE_RATE_LP;
 
     bsec_sensor_configuration_t required_sensor_settings[BSEC_MAX_PHYSICAL_SENSOR];
     uint8_t n_required_sensor_settings = BSEC_MAX_PHYSICAL_SENSOR;
@@ -97,37 +95,56 @@ int BSEC_BME_loop()
     CHK_RTN_BME(bme68x_set_heatr_conf(bme680_settings.op_mode,&heater_conf,&gas_sensor));
     
     CHK_RTN_BME(bme68x_set_op_mode(bme680_settings.op_mode, &gas_sensor));
-    
-    uint32_t del_period = bme68x_get_meas_dur(bme680_settings.op_mode, &new_conf, &gas_sensor) + (heatr_conf.shared_heatr_dur * 1000);
+    if(bme680_settings.process_data == 0 ) return 0;   
+    uint32_t del_period = bme68x_get_meas_dur(bme680_settings.op_mode, &new_conf, &gas_sensor) + (heater_conf.shared_heatr_dur * 1000);
     sleep_us(del_period);
-    if(bme680_settings.process_data == 0 ) return 0;
+    sleep_ms(100);
     struct bme68x_data new_data; 
     uint8_t n_fields;
     CHK_RTN_BME(bme68x_get_data(bme680_settings.op_mode,&new_data,&n_fields,&gas_sensor));
-
     assert(new_data.status & BME68X_NEW_DATA_MSK);
 
-    bsec_input_t input[3];
-    uint8_t n_input = 3;
+    bsec_input_t input[BSEC_MAX_PHYSICAL_SENSOR];
+    uint8_t n_input = 0;
     bsec_output_t output[5];
     uint8_t n_output = 5;
 
-    bsec_library_return_t status;
-    now = duration_cast<nanoseconds>(system_clock::now().time_since_epoch()).count();
-    input[0].sensor_id = BSEC_INPUT_GASRESISTOR;
-    input[0].signal = new_data.gas_resistance;
-    input[0].time_stamp = now;
-    input[1].sensor_id = BSEC_INPUT_TEMPERATURE;
-    input[1].signal = new_data.temperature;
-    input[1].time_stamp = now;
-    input[2].sensor_id = BSEC_INPUT_HUMIDITY;
-    input[2].signal = new_data.humidity;
-    input[2].time_stamp = now;
-    status = bsec_do_steps(input, n_input, output, &n_output);
+    if (bme680_settings.process_data & BSEC_PROCESS_GAS) {
+        input[n_input].sensor_id = BSEC_INPUT_GASRESISTOR;
+        input[n_input].signal = new_data.gas_resistance;
+        input[n_input].time_stamp = now;
+        n_input++;
+        printf("gas:%f\n",new_data.gas_resistance);
+    }
+    if (bme680_settings.process_data & BSEC_PROCESS_TEMPERATURE) {
+        input[n_input].sensor_id = BSEC_INPUT_TEMPERATURE;
+        input[n_input].signal = new_data.temperature;
+        input[n_input].time_stamp = now;
+        n_input++;
+        input[n_input].sensor_id = BSEC_INPUT_HEATSOURCE;
+        input[n_input].signal = 8.0f;
+        input[n_input].time_stamp = now;
+        n_input++;
+        printf("temp:%f\n",new_data.temperature );
+    }
+    if (bme680_settings.process_data & BSEC_PROCESS_HUMIDITY) {
+        input[n_input].sensor_id = BSEC_INPUT_HUMIDITY;
+        input[n_input].signal = new_data.humidity ;
+        input[n_input].time_stamp = now;
+        n_input++;
+        printf("humidity:%f\n",new_data.humidity );
+    }
+    if (bme680_settings.process_data & BSEC_PROCESS_PRESSURE) {
+        input[n_input].sensor_id = BSEC_INPUT_PRESSURE;
+        input[n_input].signal = new_data.pressure;
+        input[n_input].time_stamp = now;
+        n_input++;
+    }
+    bsec_library_return_t status = bsec_do_steps(input, n_input, output, &n_output);
     assert(status == BSEC_OK);
     for (int i = 0; i < n_output; i++)
     {
-        printf("%i:%i\n",output[i].sensor_id,output[i].signal);
+        printf("%i:%.2f\n",output[i].sensor_id,output[i].signal);
     }
     return 0;
 }
